11b29c216f42525d27746cf3e5a2cb0ce156e058
Nowa <nowazhu@gmail.com>
2009-12-23 04:27:15 +0800

    -

--- app/controllers/tapi/welcome_controller.rb
+++ app/controllers/tapi/welcome_controller.rb
@@ -7,17 +7,22 @@ class Tapi::WelcomeController < ApplicationController
     ori = params[:ori].join('/')
     
     result = ""
+    basic_auth = ""
     authenticate_with_http_basic do |username, password|
       logger.info { "http basic username: #{username}\nhttp basic password: #{password}" }
-      if request.post?
-        logger.info { "p: #{hash_to_querystring(params)}" }
-        result = `curl -d "#{hash_to_querystring(params)}" http://#{username}:#{password}@twitter.com/#{ori}`
-        logger.info { "curl: curl -d \"#{hash_to_querystring(params)}\" http://#{username}:#{password}@twitter.com/#{ori}" }
-      else
-        result = `curl http://#{username}:#{password}@twitter.com/#{ori}`
-        logger.info { "curl: curl http://#{username}:#{password}@twitter.com/#{ori}" }
-      end
+      
+      basic_auth = "#{username}:#{password}@"
     end
+    
+    if request.post?
+      logger.info { "p: #{hash_to_querystring(params)}" }
+      result = `curl -d "#{hash_to_querystring(params)}" http://#{basic_auth}twitter.com/#{ori}`
+      logger.info { "curl: curl -d \"#{hash_to_querystring(params)}\" http://#{basic_auth}twitter.com/#{ori}" }
+    else
+      result = `curl http://#{basic_auth}twitter.com/#{ori}`
+      logger.info { "curl: curl http://#{basic_auth}twitter.com/#{ori}" }
+    end
+    
     logger.info { "result: #{result}" }
     render :text => result
   end
