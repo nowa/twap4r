f199522c3248340d3ef58a034e1e2e9b08c72cdd
Nowa <nowazhu@gmail.com>
2009-12-23 03:35:03 +0800

    -

--- app/controllers/tapi/welcome_controller.rb
+++ app/controllers/tapi/welcome_controller.rb
@@ -8,11 +8,11 @@ class Tapi::WelcomeController < ApplicationController
       logger.info { "http basic username: #{username}\nhttp basic password: #{password}" }
       if request.post?
         logger.info { "p: #{hash_to_querystring(params)}" }
-        result = `curl -d "#{hash_to_querystring(params)}" http://twitter.com#{params[:ori]}`
-        logger.info { "curl: curl -d \"#{hash_to_querystring(params)}\" http://twitter.com#{params[:ori]}" }
+        result = `curl -d "#{hash_to_querystring(params)}" http://#{username}:#{password}@twitter.com#{params[:ori]}`
+        logger.info { "curl: curl -d \"#{hash_to_querystring(params)}\" http://#{username}:#{password}@twitter.com#{params[:ori]}" }
       else
-        result = `curl http://twitter.com#{params[:ori]}`
-        logger.info { "curl: curl http://twitter.com#{params[:ori]}" }
+        result = `curl http://#{username}:#{password}@twitter.com#{params[:ori]}`
+        logger.info { "curl: curl http://#{username}:#{password}@twitter.com#{params[:ori]}" }
       end
     end
     logger.info { "result: #{result}" }
