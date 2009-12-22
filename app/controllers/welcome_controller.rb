6b2f3c8cf709e16207c3e55579755e432ca15cda
nowa <nowazhu@gmail.com>
2009-12-22 22:20:45 +0800

    -

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,22 +1,28 @@
 class WelcomeController < ApplicationController
   
   def index
-    logger.info { "#{@client.friends_timeline}" }
+    logger.info { "#{@client.friends_timeline}" } if @client.authorized?
   end
   
   def oauth_callback
     access_token = @client.authorize(
           session[:request_token],
-          session[:request_token_secret]
+          session[:request_token_secret],
+          :oauth_verifier => params[:oauth_verifier]
     )
     
-    File.open(@config_file, 'w') do |out|
-      @config['oauth']['request_token'] = access_token.token
-      @config['oauth']['request_secret'] = access_token.secret
-      YAML::dump(@config, out)
-    end
+    if @client.authorized?
     
-    redirect_to root_path
+      File.open(@config_file, 'w') do |out|
+        @config['oauth']['request_token'] = access_token.token
+        @config['oauth']['request_secret'] = access_token.secret
+        YAML::dump(@config, out)
+      end
+    
+      redirect_to root_path and return
+    else
+      
+    end
   end
   
 end
